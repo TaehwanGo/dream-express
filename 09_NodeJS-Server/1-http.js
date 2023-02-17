fb514a4455ebdf2e82cc9e08de81008213b37397
Tony <gth1123@naver.com>
2023-02-17 21:36:50 +0900

    9.3 stream으로 응답 보내주기

--- 09_NodeJS-Server/1-http.js
+++ 09_NodeJS-Server/1-http.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const fs = require("fs");
 /**
  * https와 함께 적용이 됨
  * https를 위한 SSL 인증서(certification)가 필요함
@@ -16,26 +17,18 @@ const server = http.createServer((req, res) => {
   console.log(req.url);
   const url = req.url;
   if (url === "/") {
-    // HTML을 전달해보자
     res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>My First Page</title></head>");
-    res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
-    res.write("</html>");
+    // nodejs stream을 이용해서 ./html/output.html을 읽어서 전달해보자
+    const readStream = fs.createReadStream("./html/index.html");
+    readStream.pipe(res);
   } else if (url === "/courses") {
     res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>My Courses</title></head>");
-    res.write("<body><ul><li>Node.js</li><li>React</li></ul></body>");
-    res.write("</html>");
+    fs.createReadStream("./html/courses.html").pipe(res);
   } else {
     res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>Not Found</title></head>");
-    res.write("<body><h1>Page not found!</h1></body>");
-    res.write("</html>");
+    fs.createReadStream("./html/not-found.html").pipe(res);
   }
-  res.end();
+  // res.end(); // stream을 이용하면 end를 호출하면 안됨
 });
 
 server.listen(8080);
