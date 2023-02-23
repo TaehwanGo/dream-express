a4d630c55278b3838dfa7e506a29cacd533fb9b9
Tony <gth1123@naver.com>
2023-02-23 22:51:41 +0900

    express res

--- 11_Express/app.js
+++ 11_Express/app.js
@@ -4,9 +4,13 @@ const app = express();
 app.get("/sky/:id", (req, res, next) => {
   // console.log(req.path);
   // console.log(req.headers)
-  console.log(req.params); // path parameter -> object 형태로 key, value로 저장
-  console.log(req.query); // query parameter -> object 형태로 key, value로 저장
-  res.send("hi");
+  // console.log(req.params); // path parameter -> object 형태로 key, value로 저장
+  // console.log(req.query); // query parameter -> object 형태로 key, value로 저장
+  // res.send("hi");
+  // res.json({ name: "Tony" });
+  // res.status(201).send("created");
+  // res.setHeader("key", "value");
+  // res.setHeader("Content-Type", "text/html");
 });
 
 app.listen(8080);
