03d5eb0247fb6e336320849e4542dc629f6c06bf
Tony <gth1123@naver.com>
2023-03-09 22:35:40 +0900

    hash가 생성할때마다 다른데 compare는 true

--- 15_Auth/1-bcrypt.js
+++ 15_Auth/1-bcrypt.js
@@ -7,4 +7,5 @@ const hashed = bcrypt.hashSync(password, 10);
 console.log("hashed: ", hashed);
 
 const result = bcrypt.compareSync("abcd1234", hashed);
+// const result = bcrypt.compareSync("abcd1234", "$2b$10$TuxPqU7uFRpq9qyIWEXHg.hQvb59H/P1IHot7mpmt1JAW2KrIMzl.");
 console.log("result: ", result);
