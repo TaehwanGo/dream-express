415592faaf771ac968a8c5dc372624fced339845
Tony <gth1123@naver.com>
2023-03-18 00:41:33 +0900

    21.4 Sequelize 적용하기 - Auth

--- 21_Sequelize/readme.md
+++ 21_Sequelize/readme.md
@@ -15,3 +15,43 @@
 ```bash
 npm install --save sequelize
 ```
+
+## 21.3 Sequelize 드위터에 전용하기 - 연결
+
+## 21.4 Sequelize 드위터에 적용하기 - Auth
+
+- Auth를 Sequelize로 바꿔보자
+
+### users 테이블 만들기
+
+```js
+const User = sequelize.define("user", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  username: {
+    type: DataTypes.STRING(45),
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING(128),
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.STRING(128),
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING(128),
+    allowNull: false,
+  },
+  url: DataTypes.TEXT,
+});
+```
+
+```sql
+CREATE TABLE IF NOT EXISTS `users` (`id` INTEGER NOT NULL auto_increment , `username` VARCHAR(45) NOT NULL, `password` VARCHAR(128) NOT NULL, `name` VARCHAR(128) NOT NULL, `email` VARCHAR(128) NOT NULL, `url` TEXT, `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB;
+```
--- Dwitter/backend/data/auth.js
+++ Dwitter/backend/data/auth.js
@@ -1,33 +1,77 @@
 import { dbConnection } from "../app.js";
+import SQ from "sequelize";
+import { sequelize } from "../db/database.js";
+
+const DataTypes = SQ.DataTypes;
+
+/**
+ * user로 이름을 지으면 users로 s가 붙어서 생성된다
+ */
+const User = sequelize.define(
+  "user",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING(45),
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+    },
+    url: DataTypes.TEXT,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export async function findByUsername(username) {
-  return dbConnection
-    .execute("SELECT * FROM users WHERE username=?", [username])
-    .then((result) => {
-      console.log(result);
-      return result[0][0];
-    });
+  return User.findOne({ where: { username } });
+  // return dbConnection
+  //   .execute("SELECT * FROM users WHERE username=?", [username])
+  //   .then((result) => {
+  //     console.log(result);
+  //     return result[0][0];
+  //   });
 }
 
 export async function findById(id) {
-  console.log("findById", id);
-  return dbConnection
-    .execute("SELECT * FROM users WHERE id=?", [id])
-    .then((result) => {
-      console.log(result);
-      return result[0][0];
-    });
+  return User.findByPk(id);
+  // return dbConnection
+  //   .execute("SELECT * FROM users WHERE id=?", [id])
+  //   .then((result) => {
+  //     console.log(result);
+  //     return result[0][0];
+  //   });
 }
 
 export async function createUser(user) {
-  const { username, password, name, email, url } = user;
-  dbConnection
-    .execute(
-      "INSERT INTO users (username, password, name, email, url) VALUES (?, ?, ?, ?, ?)",
-      [username, password, name, email, url]
-    )
-    .then((result) => {
-      console.log(result);
-      return result[0].insertId;
-    });
+  return User.create(user).then((data) => {
+    console.log(data);
+    return data.dataValues.id;
+  });
+  // const { username, password, name, email, url } = user;
+  // dbConnection
+  //   .execute(
+  //     "INSERT INTO users (username, password, name, email, url) VALUES (?, ?, ?, ?, ?)",
+  //     [username, password, name, email, url]
+  //   )
+  //   .then((result) => {
+  //     console.log(result);
+  //     return result[0].insertId;
+  //   });
 }
